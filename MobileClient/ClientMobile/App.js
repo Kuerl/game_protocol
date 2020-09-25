029e6a03b18e493ae388137d7839cef760ddef4b
Tran Tan Tai <65342718+Kuerl@users.noreply.github.com>
2020-09-25 08:19:15 +0700

    mobile update

--- MobileClient/ClientMobile/App.js
+++ MobileClient/ClientMobile/App.js
@@ -1,114 +1,147 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from "react";
+import { TextInput, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import io from "socket.io-client/dist/socket.io.js";
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.socket = io("http://192.168.137.1:3000");
+    this.state = {
+      // randomNumber
+      NumberHolder: '#',
+      // stringHandle
+      string: {
+        stringSend: "This is an object to handle the character!",
+        sCount: 0,
+      },
+      // actionHandle
+      action: {
+        actionSend: "Took action!",
+        aCount: 0,
+      },
+    }
+  }
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  // Handle Random Number
+  createRandomNumber = () => {
+    let randomNumber = Math.floor(Math.random()*6) + 1;
+    this.setState({
+      NumberHolder: randomNumber,
+    });
+    this.socket.emit("send-random-number", this.state.NumberHolder);
+  }
+  // Handle String
+  sendAndCountSendString = () => {
+    this.setState({
+      string: {
+        sCount: this.state.string.sCount + 1,
+      }
+    })
+    this.socket.emit("send-string",this.state.string.stringSend);
+  }
+  // Handle Action
+  sendAndCountSendAction = () => {
+    this.setState({
+      action: {
+        aCount: this.state.action.aCount + 1,
+      }
+    })
+    this.socket.emit("send-action",this.state.action.actionSend);
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.contentContainer}>
+          <Text style={styles.content}>Testing Client!</Text>
+          <View style={styles.btnArea}>
+            <View style={styles.rArea}>
+              <Text style={styles.rTextArea}>{this.state.NumberHolder} </Text>
+              <TouchableOpacity style={styles.handleBtn} onPress={this.createRandomNumber}>
+                <Text style={styles.btnContent}>Send Random Number!</Text>
+              </TouchableOpacity>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+            <View style={styles.sArea}>
+              <Text style={styles.sTextArea}>String here: The ({this.state.string.sCount}) time(s).</Text>
+              <TouchableOpacity style={styles.handleBtn} onPress={this.sendAndCountSendString}>
+                <Text style={styles.btnContent}>Send "String"! </Text>
+              </TouchableOpacity>
+              </View>
+            <View style={styles.sArea}>
+              <Text style={styles.sTextArea}>Action Here: The ({this.state.action.aCount}) time(s).</Text>
+              <TouchableOpacity style={styles.handleBtn} onPress={this.sendAndCountSendAction}><Text style={styles.btnContent}>Send Action!</Text>
+            </TouchableOpacity>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    ...StyleSheet.absoluteFill,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#333333"
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  contentContainer: {
+    backgroundColor: '#555555',
+    height: 500,
+    width: 350,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
   },
-  body: {
-    backgroundColor: Colors.white,
+  content: {
+    color: 'white',
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+        btnArea: {
+          flex: 1,
+          alignItems: 'center',
+          margin: 10,
+          justifyContent: 'space-around'
+        },
+              handleBtn: {
+                marginHorizontal: 10,
+                marginVertical: 10,
+                backgroundColor: '#00BFFF',
+                paddingHorizontal: 10,
+                paddingVertical: 10,
+                borderRadius: 10,
+                width: 250,
+                marginLeft: 0,
+              },
+                    btnContent: {
+                      textAlign: 'center',
+                      color: 'white',
+                      fontStyle: 'italic',
+                      fontSize: 20,
+                    },
+              rArea: {
+                flex: 1,
+                flexDirection: 'row',
+                alignItems: "center",
+              },
+                    rTextArea: {
+                      fontSize: 30,
+                      marginLeft: 20,
+                      color: 'white',
+                    },
+              sArea: {
+                flex: 1,
+                flexDirection: 'column',
+                alignItems: 'center',
+              },
+                    sTextArea: {
+                      fontSize: 15,
+                      marginLeft: 20,
+                      color: 'white',
+                    },
 });
-
-export default App;
--- Server/app.js
+++ Server/app.js
@@ -1,17 +1,23 @@
-var express = require('express')
-var app = express()
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const port = 3000;
 
-// static files
-// var path = require('path');
-// app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+io.on("connection", socket => {
+  console.log("A device connected to Express server. Id: "+socket.id);
+  // random number re
+  socket.on("send-random-number", function(data){
+    console.log("Re a random number: "+data);
   })
+  // string re
+  socket.on("send-string", function(data){
+    console.log("Re a string: "+data);
+  })
+  // action re
+  socket.on("send-string", function(data){
+    console.log("Re a string: "+data);
+  })
+});
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    
-    console.log("SERVER STARTING AT: http://%s:%s", host, port)
-})
+server.listen(port, () => console.log("Server START at PORT: " + port));
